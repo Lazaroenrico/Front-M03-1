ddeb73d5c23c1a8d903261529cd1c82e82eb0070
Lázaro Enrico <lazaro.enr.menezes@gmail.com>
2022-04-16 16:35:13 -0300

    Update

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,53 @@
-const baseURL = "http://localhost:4900/console";
+const baseURL = "http://localhost:3000/consoler";
 
 async function findAllConsole() {
-  const response = await fetch(`${baseURL}/find-console`);
+  const response = await fetch(`${baseURL}/find-consoler`);
 
   const consoles = await response.json();
 
-  consoles.forEach(function (console) {
+  consoles.forEach(function (consoler) {
     document.querySelector("#cardList").insertAdjacentHTML(
-      "beforend",
+      "beforeend",
       `
       <div class="card-list">
-        <div>
-            <div class="card-name">${console.name}</div>
-            <div class="card-price">${console.price}</div>
-            <div class="card-description">${console.description}</div>
+        <div class="card-container">
+            <div class="card-name">${consoler.name}</div>
+            <div class="card-description">${consoler.description}</div>
+            <div class="card-price"> R$ ${consoler.price}</div>
         </div>
 
-        <img class="card-img" src="${console.image}" alt="">
+        <img class="card-img" src="${consoler.image}" alt="${consoler.name}">
 
-    </div>
+      </div>
 
       `
     );
   });
 }
+async function findByIdConsole(){
+
+  const id = document.querySelector("#idConsole").value
+
+  const response = await fetch(`${baseURL}/find-consoler/${id}`);
+  const consoler = await response.json();
+
+  const consoleChosenDiv = document.querySelector("#consoleChosen")
+
+  consoleChosenDiv.innerHTML = `
+  <div class="card2-list">
+
+    <div class="card2-container">
+
+      <div class="card2-name">${consoler.name}</div>
+      <div class="card2-description">${consoler.description}</div>
+      <div class="card2-price"> R$ ${consoler.price}</div>
+
+    </div>
+
+    <img class="card-img" src="${consoler.image}" alt="${consoler.name}">
+
+  </div>
+  `
+}
+
+findAllConsole();
